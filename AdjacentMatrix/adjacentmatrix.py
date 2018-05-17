0f8eee71453edf39409585a4c60e4c487a0d81c4
belalmoh <belal.mohamed@acu.edu.eg>
2018-05-18 01:13:52 +0200

    [ADDED] docs to functions.

--- AdjacentMatrix/adjacentmatrix.py
+++ AdjacentMatrix/adjacentmatrix.py
@@ -2,14 +2,30 @@ class AdjacentMatrix:
     UNIDIRECTIONAL = 0
     BIODIRECTIONAL = 1
     def __init__(self, numberOfEdges, type=UNIDIRECTIONAL):
+        '''
+
+        :param numberOfEdges: is the number of the matrix.
+        :param type: whether AdjacentMatrix.UNIDIRECTIONAL or AdjacentMatrix.BIODIRECTIONAL which are expressive enough.
+        '''
         self._Matrix = [[0 for x in range(numberOfEdges)] for y in range(numberOfEdges)]
         self._type = type
 
     def getMatrix(self):
+        '''
+        this function is responsible for printing the whole matrix in a readable way.
+        :return: void
+        '''
         for i, item in enumerate(self._Matrix):
             print(self._Matrix[i])
 
     def setPath(self, fromNode, toNode, weight=1):
+        '''
+
+        :param fromNode: from node
+        :param toNode: to node
+        :param weight: weight of the edge
+        :return: void
+        '''
         if self._type == AdjacentMatrix.UNIDIRECTIONAL:
             if fromNode != toNode:
                 self._Matrix[fromNode][toNode] = weight
